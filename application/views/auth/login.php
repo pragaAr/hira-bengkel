69eee638311c8e1ebe439037778e2f9e8a197446
praga arya <aryapragar@gmail.com>
2024-10-02 08:41:48 +0700

    ubah view dikit, add 1 svg bg and remove unused img

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -5,23 +5,23 @@
 
 
 
-<body class="d-flex align-items-center justify-content-center vh-100" style="background-image:url(<?= base_url('public/img/bg-img.jpg') ?>); background-size:cover; background-repeat:no-repeat;">
+<body class="d-flex align-items-center justify-content-center vh-100" style="background-image:url(<?= base_url('public/img/bg-img.svg') ?>); background-size:cover; background-repeat:no-repeat;">
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-5">
         <div class="card">
           <div class="card-body">
-            <div class="py-4 px-3">
+            <div class="p-3">
               <div class="text-center">
-                <h2 class="text-gray-900 font-weight-bold mb-4">Silahkan Login</h2>
+                <h3 class="text-dark font-weight-bold mb-4"><u>SIlahkan login dahulu</u></h3>
               </div>
               <form class="user" action="<?= base_url('auth') ?>" method="POST">
                 <div class="form-group">
-                  <label class="text-dark font-weight-bold" for="username">Username</label>
+                  <label class="text-dark" for="username">Username</label>
                   <input type="text" class="form-control" name="username" id="username" required autofocus autocomplete="off">
                 </div>
                 <div class="form-group">
-                  <label class="text-dark font-weight-bold" for="password">Password</label>
+                  <label class="text-dark" for="password">Password</label>
                   <input type="password" class="form-control" name="password" id="password" required>
                 </div>
                 <div class="form-group mt-4">
@@ -32,11 +32,8 @@
               </form>
 
             </div>
-            <div class="text-center mb-3">
-              <span class="text-secondary font-sm">Belum punya akun ? Hubungi Admin</span><br>
-
-              Made With 💖 <?= date('Y') ?>
-
+            <div class="text-center mb-2">
+              <small> Hira Express <br> Made With 💖 <?= date('Y') ?></small>
             </div>
           </div>
         </div>
